7025915e62db67271c4f1df5cd6d14380b9b47af
Shingo Tamura <shingokko@gmail.com>
2016-01-31 00:04:06 +1300

    Hook new graphics up with some scenes

---
+++ Assets/Scripts/Outline.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Outline : MonoBehaviour {
+	public float offsetX;
+
+	void SetText(GameObject obj) {
+		obj.GetComponent<TextMesh>().text = GetComponent<TextMesh>().text;
+	}
+
+	void Start() {
+		if (offsetX != 0) {
+			transform.Translate(new Vector3(offsetX, 0, 0));
+		}
+
+		var above = transform.Find("Above").gameObject;
+		var below = transform.Find("Below").gameObject;
+		var right = transform.Find("Right").gameObject;
+		var left = transform.Find("Left").gameObject;
+
+		SetText(above);
+		SetText(below);
+		SetText(right);
+		SetText(left);
+	}
+}
