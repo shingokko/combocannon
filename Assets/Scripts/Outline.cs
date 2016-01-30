04a65661332c71af52e83612d2b05b49172527a2
PastaTime <arran94@gmail.com>
2016-01-31 08:48:31 +1300

    Updated outline to be dynamic

--- Assets/Scripts/Outline.cs
+++ Assets/Scripts/Outline.cs
@@ -3,24 +3,38 @@ using System.Collections;
 
 public class Outline : MonoBehaviour {
 	public float offsetX;
-
+	private GameObject above;
+	private GameObject below;
+	private GameObject right;
+	private GameObject left;
 	void SetText(GameObject obj) {
 		obj.GetComponent<TextMesh>().text = GetComponent<TextMesh>().text;
+	}
+
+	string GetText(GameObject obj) {
+		return obj.GetComponent<TextMesh>().text;
 	}
 
 	void Start() {
 		if (offsetX != 0) {
 			transform.Translate(new Vector3(offsetX, 0, 0));
 		}
-
-		var above = transform.Find("Above").gameObject;
-		var below = transform.Find("Below").gameObject;
-		var right = transform.Find("Right").gameObject;
-		var left = transform.Find("Left").gameObject;
-
+		above = transform.Find("Above").gameObject;
+		below = transform.Find("Below").gameObject;
+		right = transform.Find("Right").gameObject;
+		left = transform.Find("Left").gameObject;
 		SetText(above);
 		SetText(below);
 		SetText(right);
 		SetText(left);
+	}
+
+	void Update() {
+		if (!GetText(above).Equals(GetComponent<TextMesh>().text)) {
+			SetText(above);
+			SetText(below);
+			SetText(right);
+			SetText(left);
+		}
 	}
 }
