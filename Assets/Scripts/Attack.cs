d8130fc20bc0131ab763a1441390068f4e812b4d
JeffMmc <jeffmc196@gmail.com>
2016-01-30 17:02:39 +1300

    Singleton class and enemy actions

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attack : MonoBehaviour {
+
+	GameObject enemy = null;
+	public int respawnTime = 300;
+	private int counter;
+
+	void OnMouseDown() {
+		if(EnemyHealth.Instance.currentHealth > 0){
+			EnemyHealth.Instance.currentHealth -= 1;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		enemy = (GameObject)Resources.Load("Enemy");
+		Instantiate(enemy, new Vector3(-0.4325213f, 0.4685652f, 0), Quaternion.identity);
+		EnemyHealth.Instance.currentHealth = 10;
+		counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(EnemyHealth.Instance.currentHealth == 0){
+			counter += 1;
+
+			if(counter == respawnTime){
+				enemy = (GameObject)Resources.Load("Enemy");
+				Instantiate(enemy, new Vector3(-0.4325213f, 0.4685652f, 0), Quaternion.identity);
+				counter = 0;
+			}
+			
+		}
+	}
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+	private int counter = 0;
+	
+	// Use this for initialization
+	void Start () {
+		EnemyHealth .Instance.currentHealth = 10;
+		counter = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (EnemyHealth.Instance.currentHealth == 0){
+			Destroy(gameObject);
+			counter = 0;
+		}else if(PlayerHealth.Instance.currentHealth == 0){
+			transform.localScale = new Vector3(0.48563f, 0.48563f,0.48563f);
+			counter = 0;
+		}else{
+			transform.localScale += new Vector3(0.00167f, 0.00167f,0.00167f);
+			counter += 1;
+		}
+
+		if(counter == 300 && PlayerHealth.Instance.currentHealth != 0){
+			PlayerHealth.Instance.currentHealth -= 2;
+			counter = 0;
+			transform.localScale = new Vector3(0.48563f, 0.48563f,0.48563f);
+		}
+	}
+}
