bf1c71b75f14691b4217833dc4320dc72d0f12dc
Shingo Tamura <shingokko@gmail.com>
2016-01-31 15:27:43 +1300

    Implement smoke effect

--- Assets/Scripts/Smoke.cs
+++ Assets/Scripts/Smoke.cs
@@ -1,21 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Animator))]
 public class Smoke : MonoBehaviour {
 	float _currentTime;
-	float _waitTime = 0.2f;
+	float _waitTime = 2.5f;
 	GameObject _target;
+	Animator _animator;
 
 	void Start() {
 		_currentTime = 0;
+		
 		_target = GameObject.Find("GunTip");
-		transform.position = _target.transform.position;
+		_animator = GetComponent<Animator>();
+
+    	var offsetX = ((float)Random.Range(-100, 100)) * 0.01f;
+    	var offsetY = ((float)Random.Range(-100, 100)) * 0.01f;
+
+    	transform.position = new Vector3
+		(
+			_target.transform.position.x + -0.5f,
+			_target.transform.position.y + 0.5f,
+			_target.transform.position.z
+		);
+
+		_animator.Play("smoke-expand");
 	}
 
 	void Update () {
         _currentTime += Time.deltaTime;
         if (_currentTime > _waitTime) {
-        	GetComponent<ParticleSystem>().enableEmission = false;
         	Destroy(gameObject, 0.2f);
         }
 	}
